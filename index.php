91868184ca265dfe3013fd9c35d3b1e43853ae9e
leiidyapop <49569411+leiidyapop@users.noreply.github.com>
2019-05-31 13:33:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php
   session_start();
-
   require 'database.php';
-
   if (isset($_SESSION['user_id'])) {
     $records = $conn->prepare('SELECT id, email, password FROM users WHERE id = :id');
     $records->bindParam(':id', $_SESSION['user_id']);
     $records->execute();
     $results = $records->fetch(PDO::FETCH_ASSOC);
-
     $user = null;
-
     if (count($results) > 0) {
       $user = $results;
     }
@@ -23,21 +19,24 @@
     <meta charset="utf-8">
     <title>Welcome to you WebApp</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=ZCOOL+KuaiLe&display=swap" rel="stylesheet">
+    <link rel ="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
     <?php require 'partials/header.php' ?>
 
     <?php if(!empty($user)): ?>
-      <br> Welcome. <?= $user['email']; ?>
+      <br> <h6>¡Welcome! </h6> <h5> <?= $user['email']; ?> <i class="fas fa-child"></i></h5>
       <br>You are Successfully Logged In
+      <br>
       <a href="logout.php">
-        Logout
+         Logout
       </a>
     <?php else: ?>
       <h1>Please Login or SignUp</h1>
 
-      <a href="login.php">Login</a> or
+      <a href="login.php">Login</a> or &nbsp &nbsp
       <a href="signup.php">SignUp</a>
     <?php endif; ?>
   </body>
